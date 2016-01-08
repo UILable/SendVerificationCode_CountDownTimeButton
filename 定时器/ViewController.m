4611847bcc9813883af0761c57e7d5511ef0941a
UILabel <sp0302@126.com>
2016-01-08 23:58:04 +0800

    封装了gcd 可以用xib的封装

--- 定时器/ViewController.m
+++ 定时器/ViewController.m
@@ -6,15 +6,18 @@
 
 #import "ViewController.h"
 #import "CountDownTimeButton.h"
+#import "UIButton+CountDown.h"//gcd
 @interface ViewController ()
 @property(strong,nonatomic)CountDownTimeButton *countBtn;
+@property (weak, nonatomic) IBOutlet UIButton *countdownBtn;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _countBtn=[[CountDownTimeButton alloc]initWithFrame:CGRectMake(100, 200, 100, 30) WithBtnTitle:@"点击获取验证码"];
+    _countBtn=[[CountDownTimeButton alloc]initWithFrame:CGRectMake(100, 100, 100, 30) WithBtnTitle:@"点击获取验证码"];
     
     [_countBtn addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_countBtn];
@@ -23,6 +26,9 @@
 -(void)click
 {
     [_countBtn startWithSecond:5];
+}
+- (IBAction)countDownClick:(id)sender {
+       [_countdownBtn startWithTime:5 title:@"获取验证码" countDownTitle:@"s" mainColor:[UIColor colorWithRed:84/255.0 green:180/255.0 blue:98/255.0 alpha:1.0f] countColor:[UIColor lightGrayColor]];
 }
 
 - (void)didReceiveMemoryWarning {
